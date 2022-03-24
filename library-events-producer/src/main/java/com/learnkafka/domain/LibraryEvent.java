1740a99c99611cab59ff1e5013a86500de0098a5
Shubham Gupta <shubhamgupta2496@gmail.com>
2022-03-24 17:25:22 +0530

    Initial commit

---
+++ library-events-producer/src/main/java/com/learnkafka/domain/LibraryEvent.java
@@ -0,0 +1,24 @@
+package com.learnkafka.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import javax.validation.Valid;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class LibraryEvent {
+
+    private Integer libraryEventId;
+    private LibraryEventType libraryEventType;
+    @NotNull
+    @Valid
+    private Book book;
+
+}
